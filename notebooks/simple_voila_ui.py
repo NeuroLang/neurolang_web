408236ce9626212d7d2e04af18f3c5f3dea2b3b1
Romain PRIMET <romain.primet@inria.fr>
2020-03-19 09:59:21 +0100

    extract util to module

--- notebooks/simple_voila_ui.py
+++ notebooks/simple_voila_ui.py
@@ -29,6 +29,8 @@ from neurolang.datalog.wrapped_collections import (
 )  # type: ignore
 from neurolang.frontend import NeurolangDL, ExplicitVBR  # type: ignore
 
+from nlweb.util import debounce
+
 from nilearn import datasets, plotting  # type: ignore
 import nibabel as nib  # type: ignore
 
@@ -83,50 +85,6 @@ def run_query(nl, query):
 
 
 # ## UI utilities
-
-
-# +
-import asyncio
-
-
-class Timer:
-    def __init__(self, timeout, callback):
-        self._timeout = timeout
-        self._callback = callback
-        self._task = asyncio.ensure_future(self._job())
-
-    async def _job(self):
-        await asyncio.sleep(self._timeout)
-        self._callback()
-
-    def cancel(self):
-        self._task.cancel()
-
-
-def debounce(wait):
-    """ Decorator that will postpone a function's
-        execution until after `wait` seconds
-        have elapsed since the last time it was invoked. """
-
-    def decorator(fn):
-        timer = None
-
-        def debounced(*args, **kwargs):
-            nonlocal timer
-
-            def call_it():
-                fn(*args, **kwargs)
-
-            if timer is not None:
-                timer.cancel()
-            timer = Timer(wait, call_it)
-
-        return debounced
-
-    return decorator
-
-
-# -
 
 
 class PapayaWidget(HTML):
