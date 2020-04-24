af4d231337f1b41565e46d2a138620015c6ce0c7
Hande Gözükan <hande.gozukan@inria.fr>
2020-04-24 19:03:09 +0200

    use NlIconTab from neurolang_ipywidgets; a small bug to be fixed for initial icon visualization in tab titles

--- notebooks/simple_voila_ui.py
+++ notebooks/simple_voila_ui.py
@@ -38,7 +38,7 @@ from neurolang.datalog.wrapped_collections import (
 from neurolang.frontend import NeurolangDL, ExplicitVBR  # type: ignore
 from neurolang.frontend.neurosynth_utils import StudyID, TfIDf
 
-from neurolang_ipywidgets import NlLink, NlProgress, NlCheckbox
+from neurolang_ipywidgets import NlLink, NlProgress, NlCheckbox, NlIconTab
 
 from nilearn import datasets, plotting  # type: ignore
 import nibabel as nib  # type: ignore
@@ -502,15 +502,22 @@ class ColumnFeeder:
 
 
 class ExplicitVBRColumn(ColumnFeeder):
-    def __init__(self):
+    __ICON_ON = "eye"
+    __ICON_OFF = "eye-slash"
+
+    def __init__(self, result_tab):
         super().__init__()
+        self.result_tab = result_tab
+
         self._viewer = ViewerFactory.get_region_viewer()
 
         self._turn_on_off_btn = Button(
             tooltip="Turn on/off selected regions",
-            icon="eye",
+            icon=ExplicitVBRColumn.__ICON_ON,
             layout=Layout(width="30px", padding_top="20px"),
         )
+        self.result_tab.icon = self._turn_on_off_btn.icon
+
         self._turn_on_off_btn.on_click(self._on_turn_on_off_btn_clicked)
         self._controls.append(self._turn_on_off_btn)
 
@@ -552,7 +559,11 @@ class ExplicitVBRColumn(ColumnFeeder):
     #        self._viewer.remove(images)
 
     def _on_turn_on_off_btn_clicked(self, b):
-        turn_off = True if self._turn_on_off_btn.icon == "eye-slash" else False
+        turn_off = (
+            True
+            if self._turn_on_off_btn.icon == ExplicitVBRColumn.__ICON_OFF
+            else False
+        )
 
         images = []
         for e_widget in self.__evbr_widget_list:
@@ -562,14 +573,16 @@ class ExplicitVBRColumn(ColumnFeeder):
 
         if self._column_on:
             self._column_on = False
-            self._turn_on_off_btn.icon = "eye-slash"
+            self._turn_on_off_btn.icon = ExplicitVBRColumn.__ICON_OFF
             self._unselect_btn.disabled = True
             self._viewer.remove(images)
         else:
             self._viewer.add(images)
             self._column_on = True
-            self._turn_on_off_btn.icon = "eye"
+            self._turn_on_off_btn.icon = ExplicitVBRColumn.__ICON_ON
             self._unselect_btn.disabled = False
+
+        self.result_tab.icon = self._turn_on_off_btn.icon
 
 
 class StudIdColumn(ColumnFeeder):
@@ -605,11 +618,13 @@ class ColumnFeederFactory:
     """A factory class that creates `ColumnFeeder`s for specified column types."""
 
     @staticmethod
-    def get_column(column_type):
+    def get_column(result_tab, column_type):
         """Creates and returns a `ColumnFeeder` for the specified `column_type`.
         
         Parameters
         ----------
+        result_tab: ResultTabWidget
+            the result tab that views the required column.
         column_type: str
             type of the column for the required `ColumnFeeder`.
             
@@ -620,7 +635,7 @@ class ColumnFeederFactory:
                 
         """
         if column_type == neurolang.regions.ExplicitVBR:
-            return ExplicitVBRColumn()
+            return ExplicitVBRColumn(result_tab)
         elif column_type == neurolang.frontend.neurosynth_utils.StudyID:
             return StudIdColumn()
         elif (
@@ -635,11 +650,11 @@ class ColumnFeederFactory:
 class ColumnsManager:
     """A class that creates column feeders for a specified `tuple` of column types and manages creation of widgets for each column and, their corresponding viewers and controls. """
 
-    def __init__(self, column_types: tuple):
+    def __init__(self, result_tab, column_types: tuple):
         self.columns = []
 
         for column_type in column_types.__args__:
-            self.columns.append(ColumnFeederFactory.get_column(column_type))
+            self.columns.append(ColumnFeederFactory.get_column(result_tab, column_type))
 
     def get_cell_widget(self, index, obj):
         """Creates and returns the cell widget for the column specified by `index` and the object `obj` for that column.
@@ -683,15 +698,17 @@ class ColumnsManager:
         controls = []
         for column in self.columns:
             if column.controls is not None:
-                controls = controls + column.controls
+                controls.extend(column.controls)
         return controls
 
 
 # ### Query and result widgets
 
 
-class TableSetWidget(VBox):
+class ResultTabWidget(VBox):
     """"""
+
+    icon = Unicode()
 
     def __init__(self, name: str, wras: WrappedRelationalAlgebraSet):
         super().__init__()
@@ -701,13 +718,13 @@ class TableSetWidget(VBox):
         # create widgets
         name_label = HTML(f"<h2>{name}</h2>")
 
-        self._columns_manager = ColumnsManager(wras.row_type)
+        columns_manager = ColumnsManager(self, wras.row_type)
 
-        self.sheet = self._init_sheet(self.wras)
+        self.sheet = self._init_sheet(self.wras, columns_manager)
 
-        self.cell_viewers = self._columns_manager.get_viewers()
+        self.cell_viewers = columns_manager.get_viewers()
 
-        self.controls = self._columns_manager.get_controls()
+        self.controls = columns_manager.get_controls()
 
         if self.controls is not None:
             hbox_menu = HBox(self.controls)
@@ -721,7 +738,7 @@ class TableSetWidget(VBox):
         else:
             self.children = [name_label, self.sheet]
 
-    def _init_sheet(self, wras):
+    def _init_sheet(self, wras, columns_manager):
         column_headers = [str(i) for i in range(wras.arity)]
         rows_visible = min(len(wras), 5)
         # TODO this is to avoid performance problems until paging is implemented
@@ -736,7 +753,7 @@ class TableSetWidget(VBox):
         for i, tuple_ in enumerate(wras.unwrapped_iter()):
             row_temp = []
             for j, el in enumerate(tuple_):
-                cell_widget = self._columns_manager.get_cell_widget(j, el)
+                cell_widget = columns_manager.get_cell_widget(j, el)
                 row_temp.append(cell_widget)
             row(i, row_temp)
             # TODO this is to avoid performance problems until paging is implemented
@@ -749,45 +766,61 @@ class TableSetWidget(VBox):
 
 
 class ResultWidget(VBox):
+    """"""
+
     def __init__(self):
         super().__init__()
-
-        self.tab = Tab(layout=Layout(height="400px"))
+        self.tab = NlIconTab(layout=Layout(height="400px"))
 
     def show_results(self, res: Dict[str, WrappedRelationalAlgebraSet]):
         self.reset()
-        names, tablesets = self._create_tablesets(res)
+        names, tablesets, viewers, icons = self._create_tablesets(res)
+
+        self.children = (self.tab,) + tuple(viewers)
 
         for i, name in enumerate(names):
             self.tab.set_title(i, name)
+
         self.tab.children = tablesets
+
+        self.tab.title_icons = icons
 
     def _create_tablesets(self, res):
         answer = "ans"
 
         tablesets = []
         names = []
+        icons = []
 
         viewers = set()
 
         for name, result_set in res.items():
-            tableset_widget = TableSetWidget(name, result_set)
+            tableset_widget = ResultTabWidget(name, result_set)
 
             if name == answer:
                 tablesets.insert(0, tableset_widget)
                 names.insert(0, name)
+                icons.insert(0, tableset_widget.icon)
             else:
                 tablesets.append(tableset_widget)
                 names.append(name)
+                icons.append(tableset_widget.icon)
 
             viewers = viewers | tableset_widget.get_viewers()
 
-        self.children = self.children + tuple(viewers)
-        return names, tablesets
+            def icon_changed(change):
+                icons = []
+
+                for tableset in tablesets:
+                    icons.append(tableset.icon)
+                self.tab.title_icons = icons
+
+            tableset_widget.observe(icon_changed, names="icon")
+
+        return names, tablesets, viewers, icons
 
     def reset(self):
-        self.children = [self.tab]
-        # close all widgets
+        self.tab = NlIconTab(layout=Layout(height="400px"))
 
 
 class QueryWidget(VBox):
