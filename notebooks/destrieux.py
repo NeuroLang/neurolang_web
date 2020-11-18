9e407baef2af064edcfe085225502ca7d2591970
Hande Gözükan <hande.gozukan@inria.fr>
2020-11-18 12:07:08 +0100

    add destrieux notebook for gallery

---
+++ notebooks/destrieux.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+# ## Destrieux cortical atlas (dated 2009)
+
+# +
+import warnings  # type: ignore
+
+warnings.filterwarnings("ignore")
+
+from neurolang import regions  # type: ignore
+from neurolang.frontend import NeurolangDL, ExplicitVBR  # type: ignore
+
+from nlweb.viewers.query import QueryWidget
+
+import nibabel as nib
+
+from nilearn import datasets  # type: ignore
+
+
+# Query agent
+def init_agent():
+    """
+    Set up the neurolang query runner (?) and add facts (?) to
+    the database
+    """
+    nl = NeurolangDL()
+
+    @nl.add_symbol
+    def region_union(rs):
+        return regions.region_union(rs)
+
+    return nl
+
+
+def add_destrieux(nl):
+    nl.new_symbol(name="destrieux")
+    destrieux_atlas = datasets.fetch_atlas_destrieux_2009()
+    destrieux_atlas_image = nib.load(destrieux_atlas["maps"])
+    destrieux_labels = dict(destrieux_atlas["labels"])
+
+    destrieux_set = set()
+    for k, v in destrieux_labels.items():
+        if k == 0:
+            continue
+        destrieux_set.add(
+            (
+                v.decode("utf8"),
+                ExplicitVBR.from_spatial_image_label(destrieux_atlas_image, k),
+            )
+        )
+
+    nl.add_tuple_set(destrieux_set, name="destrieux")
+
+
+# Prepare engine
+nl = init_agent()
+add_destrieux(nl)
+
+
+# display query gui
+query = "ans(region_union(r)) :- destrieux(..., r)"
+qw = QueryWidget(nl, query)
+qw
